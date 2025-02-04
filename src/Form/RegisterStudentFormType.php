13fbe324ef82d1610f014ffa51d372abb95f4dce
DvK42 <guillaume.couvidou@live.fr>
2025-02-04 14:14:51 +0100

    Update Student register form & authenticator for login

--- src/Form/RegisterStudentFormType.php
+++ src/Form/RegisterStudentFormType.php
@@ -9,10 +9,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterStudentFormType extends AbstractType
@@ -20,19 +23,78 @@ class RegisterStudentFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, ['label' => 'Email'])
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('firstName', TextType::class, ['label' => 'Prénom'])
-            ->add('lastName', TextType::class, ['label' => 'Nom'])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => true,
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme',
+                    'Autre' => 'autre',
+                ],
+                'label' => 'Genre',
+                'expanded' => false, 
+                'required' => true,
+            ])
+            ->add('dateOfBirth', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de naissance',
+                'required' => false,
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone mobile',
+                'required' => false,
+            ])
+            ->add('studyPlace', TextType::class, [
+                'label' => 'Établissement',
+                'required' => false,
+            ])
             ->add('education', ChoiceType::class, [
-                'choices' => [EducationLevel::LEVELS],
+                'choices' => array_combine(EducationLevel::LEVELS, EducationLevel::LEVELS),
                 'placeholder' => 'Choisissez votre niveau',
                 'label' => 'Niveau actuel',
                 'required' => true,
-                ])
-            ->add('_csrf_token', HiddenType::class, [
-                'mapped' => false,
-                'data' => $options['csrf_token'],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'required' => false,
+            ])
+            ->add('addressComplement', TextType::class, [
+                'label' => 'Complément d’adresse',
+                'required' => false,
+            ])
+            ->add('postalCode', TextType::class, [
+                'label' => 'Code postal',
+                'required' => false,
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'required' => false,
+            ])
+            ->add('portfolioUrl', TextType::class, [
+                'label' => 'Site web personnel (portfolio)',
+                'required' => false,
+            ])
+            ->add('isDriver', CheckboxType::class, [
+                'label' => 'Je possède un permis de conduire',
+                'required' => false,
+            ])
+            ->add('isHandicap', CheckboxType::class, [
+                'label' => 'J’ai une forme de handicap',
+                'required' => false,
             ]);
     }
 
